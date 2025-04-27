c84650b1bc39a0e24f85c6c86a2c86e2da5e0799
zahid4kh <halilzahid@gmail.com>
2025-04-27 23:50:04 +0200

    more add; more pythagor

--- src/main/kotlin/Math.kt
+++ src/main/kotlin/Math.kt
@@ -10,10 +10,23 @@ class Math {
         return a+b
     }
 
+    fun addFloat(a: Float, b: Float): Float{
+        return a + b
+    }
+
+    fun addDouble(a: Double, b: Double): Double{
+        return a + b
+    }
+
     fun pythagor(sideA: Float, sideB: Float): Float {
         val hypotenuse = sideA.pow(2) + sideB.pow(2)
 
         return sqrt(hypotenuse)
     }
 
+    fun pythagorCathetus(sideA: Float, hypotenuse: Float): Float{
+        val sideB = hypotenuse.pow(2) - sideA.pow(2)
+
+        return sqrt(sideB)
+    }
 }
