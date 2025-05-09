6c685a29e87157321b16a1aec66989ee2eafccfd
zahid4kh <halilzahid@gmail.com>
2025-05-09 12:51:55 +0200

    checking if the rect is golden

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,12 @@
 package math
 
-fun main(){
+import math.twoD.rectangle.isGoldenRect
+import math.twoD.rectangle.makeRectGolden
 
+fun main(){
+    println(isGoldenRect(2f, 3f)) // false
+
+    println(makeRectGolden(2f, isShorterSide = true)) //3.236068
+
+    println(isGoldenRect(2f, 3.23f)) // true
 }
